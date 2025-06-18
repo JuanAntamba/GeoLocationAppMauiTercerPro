8a6ed2848ac30c4fa479fde6d99900cbce686700
laboratorio <laboratorio@UPOAULA10702>
2025-06-18 08:31:49 -0500

    Agregar archivos de proyecto.

---
+++ GeoLocationAppMauiTercerPro/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GeoLocationAppMauiTercerPro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
