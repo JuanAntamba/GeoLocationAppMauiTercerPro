ba2d8731565a09c100c2fe57cebf7071f2d740c4
Juan Carlos Antamba <juan.antamba@udla.edu.ec>
2025-06-19 00:22:03 -0500

    Modelos weather

--- GeoLocationAppMauiTercerPro/Services/WeatherServices.cs
+++ GeoLocationAppMauiTercerPro/Services/WeatherServices.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static GeoLocationAppMauiTercerPro.Models.WeatherModels;
 
 namespace GeoLocationAppMauiTercerPro.Services
 {
-    class WeatherServices
+    public class WeatherServices
     {
+        public async Task <WeatherData> GetWeatherDataFromLocationAsync(double latitude, double longitude)
+        {
+            string latitude_str = latitude.ToString().Replace(',', '.');
+            string longitude_str = longitude.ToString().Replace(',', '.');
 
+            string url = "https://api.open-meteo.com/v1/forecast?latitude="+latitude_str+"&longitude="+longitude_str+"&current=temperature_2m,relative_humidity_2m,rain`";
 
+            using(HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var resoult = await response.Content.ReadAsStringAsync();
 
+            }
+        }
     }
 }
